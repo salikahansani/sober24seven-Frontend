1675ee03c00aa4e0d1ec7e1b70cd9bab933087a2
salikahansani <126691389+salikahansani@users.noreply.github.com>
2024-03-01 03:13:00 +0530

    welcome screen changes

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,176 +1,104 @@
-import * as React from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { Image } from "expo-image";
-import LoginContainer1 from "../components/LoginContainer1";
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 import { Color, Border, FontSize, FontFamily } from "../GlobalStyles";
 
 const WelcomeScreen = () => {
-  return (
-    <View style={styles.welcomeScreen}>
-      <View style={styles.welcomeScreenChild} />
-      <LoginContainer1
-        buttonText={require("../assets/iconmonstrgithub1-1.png")}
-        actionText="Sign Up"
-        componentSize={require("../assets/gitlabseeklogocom-1.png")}
-        showGithub={false}
-        showGitlab={false}
-        showWith={false}
-        loginButtonPosition="absolute"
-        loginButtonWidth="86.71%"
-        loginButtonHeight="22.77%"
-        loginButtonTop="70.87%"
-        loginButtonRight="6.76%"
-        loginButtonBottom="6.36%"
-        loginButtonLeft="6.52%"
-        rectangleViewBackgroundColor="#46a5fc"
-        rectangleViewHeight="22.06%"
-        rectangleViewBottom="77.94%"
-        githubHeight="22.06%"
-        githubTop="57.84%"
-        groupViewTop={11}
-        groupViewHeight={23}
-        gitHubTop="8.7%"
-        loginLeft="41.23%"
-        loginColor="#fff"
-        loginTop="5.39%"
-        gitlabHeight="22.06%"
-        gitlabTop="57.84%"
-        groupViewTop1={11}
-        groupViewHeight1={22}
-        gitLabTop="9.05%"
-        withHeight="9.31%"
-        withTop="32.84%"
-        withBottom="57.84%"
-        lineViewTop="51.58%"
-        lineViewBottom="45.79%"
-        lineViewTop1="51.58%"
-        lineViewBottom1="45.79%"
-      />
-      <Text style={styles.sdgp114}>@2024 SDGP 114</Text>
-      
-      <Text style={styles.quoteText}>
-            The only person you are destined to become is the person you decide to be.
-          </Text>
+  // Fonts must be loaded before rendering the app
+  // Consider using a splash screen while fonts are loading
 
-      <Text style={[styles.createAccount, styles.createAccountFlexBox]}>
-        Create Account
-      </Text>
-      {/* <Text style={styles.exploreTheApp}>Explore the App</Text> */}
+  return (
+    <View style={styles.container}>
       <Image
-        style={styles.logoWithName1}
-        contentFit="cover"
+        source={require("../assets/ellipse-1405.png")}
+        style={styles.backgroundEllipse}
+      />
+      <Image
         source={require("../assets/logo-with-name-1.png")}
+        style={styles.logo}
       />
-      <Image
-        style={[styles.welcomeScreenInner, styles.ellipseIconLayout]}
-        contentFit="cover"
-        source={require("../assets/ellipse-1405.png")}
-      />
-      <Image
-        style={[styles.ellipseIcon, styles.ellipseIconLayout]}
-        contentFit="cover"
-        source={require("../assets/ellipse-1405.png")}
-      />
+      <Text style={styles.title}>Explore the App</Text>
+      <Text style={styles.subtitle}>
+        The only person you are destined to become is the person you decide to be.
+      </Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, styles.buttonOutline]}>
+        <Text style={[styles.buttonText, styles.buttonOutlineText]}>Create Account</Text>
+      </TouchableOpacity>
+      <Text style={styles.footer}>@2024 SDGP 114</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  createAccountFlexBox: {
-    textAlign: "center",
-    color: Color.colorBlack,
-    position: "absolute",
-  },
-  ellipseIconLayout: {
-    height: 329,
-    width: 480,
-    position: "absolute",
-  },
-  welcomeScreenChild: {
-    top: 80,
-    left: 20,
-    borderRadius: Border.br_xs,
-    width: 372,
-    height: 759,
-    position: "absolute",
-    backgroundColor: Color.colorAliceblue_300,
-  },
-  sdgp114: {
-    top: "95.87%",
-    left: "33.33%",
-    fontSize: FontSize.size_sm,
-    color: Color.colorDarkgray_200,
-    textAlign: "left",
-    fontFamily: FontFamily.manropeSemiBold,
-    fontWeight: "600",
-    position: "absolute",
-  },
-  createAccount: {
-    height: "5.36%",
-    width: "48.07%",
-    top: "76.9%",
-    left: "22.71%",
-    fontSize: FontSize.size_mid,
-    display: "flex",
+  container: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    fontFamily: FontFamily.manropeSemiBold,
-    fontWeight: "600",
-    textAlign: "center",
-  },
-  theOnlyPerson: {
-    top: 350,
-    left: 27,
-    fontSize: FontSize.size_lg,
-    letterSpacing: 0,
-    fontFamily: FontFamily.mandali,
-    width: 338,
-    height: 63,
-  },
-  exploreTheApp: {
-    height: "11.83%",
-    width: "84.06%",
-    top: "47.99%",
-    left: "5.56%",
-    fontSize: 51,
-    fontFamily: FontFamily.mansalvaRegular,
-    color: Color.colorBlack,
-    textAlign: "left",
-    position: "absolute",
-  },
-  logoWithName1: {
-    top: 153,
-    left: 116,
-    width: 159,
-    height: 138,
-    position: "absolute",
-  },
-  welcomeScreenInner: {
-    top: 732,
-    left: -39,
-  },
-  ellipseIcon: {
-    top: 335,
-    left: 1711,
-  },
-  welcomeScreen: {
-    flex: 1,
-    width: "100%",
-    height: 896,
-    overflow: "hidden",
     backgroundColor: Color.colorAliceblue_300,
   },
-    quoteText: {
-    textAlign: 'center',
+  backgroundEllipse: {
+    position: "absolute",
+    width: "100%",
+    height: "40%",
+    top: 600,
+  },
+  logo: {
+    width: 159,
+    height: 138,
+    resizeMode: "contain",
+    marginBottom:25,
+  },
+  title: {
+    fontSize: FontSize.size_10xl_6,
+    fontFamily: FontFamily.mansalvaRegular,
+    color: Color.colorBlack,
+    marginBottom: 8,
+    top:10,
+    gap:40,
+    marginBottom: 20,
+    
+  },
+  subtitle: {
     fontSize: 14,
     color: '#777777',
     fontStyle: 'italic',
-    top: 350,
-    left: 27,
-    letterSpacing: 0,
     fontFamily: FontFamily.mandali,
-    width: 338,
-    height: 63,
+    textAlign: "center",
+    paddingHorizontal: 32,
+    marginBottom: 24,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#46a5fc",
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: Border.br_xs,
+    width: "86.71%",
+    alignItems: "center",
+    marginBottom: 8,
+  
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: FontSize.size_mid,
+    fontFamily: FontFamily.manropeSemiBold,
+  },
+  buttonOutline: {
+    backgroundColor: "transparent",
+    borderWidth: 2,
+    borderColor: "#46a5fc",
+  },
+  buttonOutlineText: {
+    color: "#46a5fc",
+  },
+  footer: {
+    position: "absolute",
+    bottom: 16,
+    fontSize: FontSize.size_sm,
+    color: Color.colorDarkgray_200,
+    fontFamily: FontFamily.manropeSemiBold,
   },
 });
 
